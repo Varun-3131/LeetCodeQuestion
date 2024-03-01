5f3e5f2ca723cd9f20838e85d3a4ad5d2631175a
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-01 22:28:45 +0530

    Excel Sheet Column Title...

---
+++ src/main/java/Main/ExcelSheetColumnTitle.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        if (columnNumber == 0) return "";
+        columnNumber--;
+        char c = (char) ('A' + columnNumber % 26);
+        return convertToTitle(columnNumber / 26) + c;
+    }
+}
