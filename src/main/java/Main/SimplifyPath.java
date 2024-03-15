f43a8a5d0d48ef417fe80f67408962bc2a3e85f8
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-15 10:26:45 +0530

    Simplify Path...

---
+++ src/main/java/Main/SimplifyPath.java
@@ -0,0 +1,27 @@
+package Main;
+
+public class SimplifyPath {
+    public String simplifyPath(final String path) {
+        final StringBuilder sb = new StringBuilder();
+        final Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < path.length() + 1; ++i) {
+            if ((i == path.length() || path.charAt(i) == '/') && sb.length() > 0) {
+                final String directory = sb.toString();
+
+                if (directory.equals("..") && !stack.isEmpty()) stack.pop();
+                else if (!directory.equals("..") && !directory.equals(".")) stack.push(directory);
+
+                sb.setLength(0);
+            } else if (i < path.length() && path.charAt(i) != '/') {
+                sb.append(path.charAt(i));
+            }
+        }
+
+        String result = "";
+
+        while (!stack.isEmpty()) result = "/" + stack.pop() + result;
+
+        return result.length() == 0 ? "/" : result;
+    }
+}
