3b91893233fe57c4aaeaf4a3d894cbb48ff468c8
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-21 22:35:56 +0530

    Set Matrix Zeroes...

---
+++ src/main/java/Main/SetMatrixZeroes.java
@@ -0,0 +1,33 @@
+package Main;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        boolean frow = false,fcolumn = false;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0) frow = true;
+                    if(j == 0) fcolumn = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }}
+        }
+        if(frow) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(fcolumn) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
