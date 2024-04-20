b17fa7063cee707c182200c9c1845731eb8ce134
Varun-3131 <srivastavvarun48@gmail.com>
2024-04-20 15:55:29 +0530

    Contains Duplicate...

---
+++ src/main/java/Main/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> a = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (a.contains(nums[i])) {
+                return true;
+            }
+            a.add(nums[i]);
+        }
+        return false;
+    }
+}
