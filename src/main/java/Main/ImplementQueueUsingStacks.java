1b21724f89d558e83f3679250fcf13714c2add98
Varun-3131 <srivastavvarun48@gmail.com>
2024-05-04 23:18:31 +0530

    Implement Queue Using Stacks...

---
+++ src/main/java/Main/ImplementQueueUsingStacks.java
@@ -0,0 +1,54 @@
+package Main;
+
+public class ImplementQueueUsingStacks {
+    Stack<Integer> primaryStack;
+    Stack<Integer> secondaryStack;
+
+
+    int peek;
+
+    public MyQueue() {
+        primaryStack = new Stack<>();
+        secondaryStack = new Stack<>();
+    }
+
+    public void push(int x) {
+
+        if(primaryStack.isEmpty()) {
+            peek = x;
+        }
+
+        primaryStack.push(x);
+    }
+
+    public int pop() {
+
+        while(!primaryStack.isEmpty()){
+            secondaryStack.push(primaryStack.pop());
+        }
+
+
+        int popped = secondaryStack.pop();
+
+
+        if(!secondaryStack.isEmpty()) {
+            peek = secondaryStack.peek();
+        }
+
+
+        while(!secondaryStack.isEmpty()){
+            primaryStack.push(secondaryStack.pop());
+        }
+
+
+        return popped;
+    }
+
+    public int peek() {
+        return peek;
+    }
+
+    public boolean empty() {
+        return primaryStack.isEmpty();
+    }
+}
