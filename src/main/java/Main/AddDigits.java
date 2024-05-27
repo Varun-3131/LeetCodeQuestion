7600289f0d628d851ef51c698615ee4afa0b43bc
Varun-3131 <srivastavvarun48@gmail.com>
2024-05-27 14:30:36 +0530

    Add Digits...

---
+++ src/main/java/Main/AddDigits.java
@@ -0,0 +1,24 @@
+package Main;
+
+public class AddDigits {
+    public int addDigits(int num) {
+
+        if(num % 10 == num)
+            return num;
+        else
+            return addDigits(sum(num));
+
+    }
+
+    public int sum(int num)
+    {
+
+        int sum = 0;
+        while(num > 0)
+        {
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
