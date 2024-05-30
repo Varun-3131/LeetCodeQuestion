894fdf03691d9cc7eeb3fec8b8b0cd49ea9d63b9
Varun-3131 <srivastavvarun48@gmail.com>
2024-05-30 22:02:46 +0530

    Rotate Array...

---
+++ src/main/java/Main/RotateArray.java
@@ -0,0 +1,35 @@
+package Main;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+        int l = 0, r = nums.length - 1;
+        while(l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l += 1;
+            r -= 1;
+        }
+        l = 0;
+        r = k - 1;
+        while(l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l += 1;
+            r -= 1;
+        }
+        l = k;
+        r = nums.length - 1;
+        while(l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l += 1;
+            r -= 1;
+        }
+    }
+
+}
