f8d2b1026da5cd6263a150fc853009f69efdcb96
Varun-3131 <srivastavvarun48@gmail.com>
2024-06-11 15:33:42 +0530

    Count Complete Tree Nodes...

---
+++ src/main/java/Main/CountCompleteTreeNodes.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
