5ae76801af46f8e2d6a69c5561b3995f3011923c
Varun-3131 <srivastavvarun48@gmail.com>
2024-06-12 12:13:39 +0530

    Majority Element II...

---
+++ src/main/java/Main/MajorityElement_II.java
@@ -0,0 +1,53 @@
+package Main;
+
+public class MajorityElement_II {
+
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], map.get(nums[i]) + 1);
+            else
+                map.put(nums[i], 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            int potentialCandidate = entry.getValue();
+            if (potentialCandidate > nums.length / 3)
+                res.add(entry.getKey());
+        }
+
+        return res;
+    }
+
+    public List<Integer> majorityElement_2(int[] nums) {
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1++;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        if (count1 > nums.length / 3) res.add(candidate1);
+        if (count2 > nums.length / 3) res.add(candidate2);
+        return res;
+    }
+}
