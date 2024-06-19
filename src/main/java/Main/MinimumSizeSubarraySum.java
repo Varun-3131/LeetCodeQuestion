3cb046858c60b3b34e277bca95fe323b9fbe2ae3
Varun-3131 <srivastavvarun48@gmail.com>
2024-06-19 21:58:57 +0530

    Minimum Size Subarray Sum...

---
+++ src/main/java/Main/MinimumSizeSubarraySum.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, total = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int r = 0; r < nums.length; r++) {
+            total += nums[r];
+            while (total >= target) {
+                res = Math.min(r - l + 1, res);
+                total -= nums[l++];
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
