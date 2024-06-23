7c5f487fb8aabe23ac7d61945c3227124fd44d05
Varun-3131 <srivastavvarun48@gmail.com>
2024-06-23 19:30:10 +0530

    Missing Number...

---
+++ src/main/java/Main/MissingNumber.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int total = nums.length * (nums.length + 1) / 2;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return total - sum;
+    }
+}
