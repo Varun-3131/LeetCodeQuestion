8bc6a27a9c7a4c66314f6ad623b8c92a788f7f2f
Varun-3131 <srivastavvarun48@gmail.com>
2024-06-26 21:01:24 +0530

    Contains Duplicate II...

---
+++ src/main/java/Main/ContainsDuplicate_II.java
@@ -0,0 +1,24 @@
+package Main;
+
+public class ContainsDuplicate_II {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Set<Integer> window = new HashSet<>();
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+
+            if (window.size() > k) {
+                window.remove(nums[left]);
+                left++;
+            }
+
+            if (window.contains(nums[right])) {
+                return true;
+            }
+
+            window.add(nums[right]);
+        }
+
+        return false;
+    }
+}
