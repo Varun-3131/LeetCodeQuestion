6b2db851986a2341a6b2c688d4d2f8a4cc7a4377
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-09 22:42:07 +0530

    Valid Perfect Squares...

---
+++ src/main/java/Main/ValidPerfectSquares.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class ValidPerfectSquares {
+    public boolean isPerfectSquare(int num) {
+
+        long left = 1;
+        long right = num;
+        while (left <= right) {
+
+            long mid = (left + right) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+
+        return false;
+    }
+}
