9ea5a529675e29a5a829cd8df9c26a59952842fa
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-17 22:09:35 +0530

    Linked List Random Node...

---
+++ src/main/java/Main/LinkedListRandomNode.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class LinkedListRandomNode {
+    ArrayList<Integer>list=new ArrayList<>();
+    public Solution(ListNode head) {
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+    }
+
+    public int getRandom() {
+        double random=Math.random()*list.size();
+        return list.get((int)random);
+    }
+}
