31e94618acaf10422a134436c3721552f214d774
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-25 19:07:31 +0530

    Find The Difference...

---
+++ src/main/java/Main/FindTheDifference.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            c=c^(s.charAt(i));
+
+        }
+        for(int i=0;i<t.length();i++){
+            c=c^t.charAt(i);
+        }
+        return (char)c;
+    }
+}
