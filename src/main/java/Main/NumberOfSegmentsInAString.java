fcfca428387ec31be353c34b55994c1ddeb5fcf9
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-08 16:15:43 +0530

    Number Of Segments In A String...

---
+++ src/main/java/Main/NumberOfSegmentsInAString.java
@@ -0,0 +1,28 @@
+package Main;
+
+public class NumberOfSegmentsInAString {
+    public int countSegments(String s) {
+        if(s.length()==0)
+            return 0;
+        int i=0,x=1;
+        if(s.charAt(0)==' '){
+            i=1;x=0;
+            while(i<s.length()-1){
+                if(s.charAt(i)!=' '&&s.charAt(i+1)==' '){
+                    x++;
+                }
+                i++;
+            }
+            if(s.charAt(s.length()-1)!=' ')
+                x+=1;
+            return x;
+        }
+        while(i<s.length()-1){
+            if(s.charAt(i)==' '&&s.charAt(i+1)!=' '){
+                x++;
+            }
+            i++;
+        }
+        return x;
+    }
+}
