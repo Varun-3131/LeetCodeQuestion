58f20a65d47ab51c8f069af0bd75e740e260cf47
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-11 17:25:48 +0530

    Repeated Substring Pattern...

---
+++ src/main/java/Main/RepeatedSubstringPattern.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+        return sub.contains(s);
+    }
+}
