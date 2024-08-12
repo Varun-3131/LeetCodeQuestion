60d933bc57db111ed67636a8774d6b4292620cd6
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-12 15:32:01 +0530

    Invert Binary Tree...

---
+++ src/main/java/Main/InvertBinaryTree.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.right = invertTree(root.left);
+        node.left = invertTree(root.right);
+        return node;
+    }
+}
