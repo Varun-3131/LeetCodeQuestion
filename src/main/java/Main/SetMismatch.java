f71e4aac102446cf665310ec1d08d30c3ccf480b
Varun-3131 <srivastavvarun48@gmail.com>
2024-09-15 20:48:55 +0530

    Set Mismatch...

---
+++ src/main/java/Main/SetMismatch.java
@@ -0,0 +1,26 @@
+package Main;
+
+public class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        int[] arr = {1,1};
+        int n = nums.length;
+
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i< n ;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int i = 1; i< n+1 ;i++){
+            if(map.containsKey(i)){
+                if (map.get(i) == 2){
+                    arr[0] = i;
+                }
+            }
+            else{ // element is missing
+                arr[1] = i;
+            }
+        }
+        return arr;
+    }
+}
