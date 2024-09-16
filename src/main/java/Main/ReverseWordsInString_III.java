4692587e20b06dfeb8a28b0799b8e98b136d354e
Varun-3131 <srivastavvarun48@gmail.com>
2024-09-16 22:25:41 +0530

    Reverse Words In String III...

---
+++ src/main/java/Main/ReverseWordsInString_III.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class ReverseWordsInString_III {
+
+    static String reverse(StringBuilder s){
+        return s.reverse().toString();
+    }
+    public String reverseWords(String s) {
+        String arr[] = s.split(" ");
+
+        int n = arr.length;
+
+        StringBuilder ans = new StringBuilder("");
+
+        for(int i=0; i<n; i++){
+            StringBuilder x = new StringBuilder(arr[i]);
+            ans.append(reverse(x));
+
+            if(i!=n-1) ans.append(" ");
+        }
+        return ans.toString();
+    }
+}
