58cb3d94c463a2c5c413fc3137289c544822cd35
Varun-3131 <srivastavvarun48@gmail.com>
2024-09-17 21:24:35 +0530

    Longest Uncommon Subsequence I...

---
+++ src/main/java/Main/LongestUncommonSubsequence_I.java
@@ -0,0 +1,22 @@
+package Main;
+
+public class LongestUncommonSubsequence_I {
+    public int findLUSlength(String a, String b)
+    {
+        int la=a.length();
+        int lb=b.length();
+        if(a.equals(b)){
+            return -1;
+        }
+        else{
+            if (la>lb){
+                return la;
+            }
+            else{
+                return lb;
+            }
+
+        }
+
+    }
+}
