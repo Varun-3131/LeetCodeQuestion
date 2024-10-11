f8b18dad3acb755463cd3f35ee7207fb8b3f0a24
Varun-3131 <srivastavvarun48@gmail.com>
2024-10-11 20:50:40 +0530

    Find Missing Second Array...

---
+++ src/main/java/Main/FindMissingInSecondArray.java
@@ -0,0 +1,20 @@
+package Main;
+
+public class FindMissingInSecondArray {
+
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    {
+        ArrayList<Integer> res=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:b){
+            set.add(i);
+        }
+        for(int i:a){
+            if(!set.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
+}
