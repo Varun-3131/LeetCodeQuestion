465ed1cb2fa7ac2f8abcc86afebce4273e62fab4
Varun-3131 <srivastavvarun48@gmail.com>
2024-10-17 21:18:20 +0530

    Find Common Characters...

---
+++ src/main/java/Main/FindCommonCharacters.java
@@ -0,0 +1,25 @@
+package Main;
+
+public class FindCommonCharacters {
+    public List<String> commonChars(String[] words) {
+        List<String> l1 = new ArrayList<>();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int minCount = Integer.MAX_VALUE;
+            for (String word : words) {
+                int count = 0;
+                for (char c : word.toCharArray()) {
+                    if (c == ch) {
+                        count++;
+                    }
+                }
+                minCount = Math.min(minCount, count);
+            }
+            for (int i = 0; i < minCount; i++) {
+                String s = String.valueOf(ch);
+                l1.add((s));
+            }
+        }
+
+        return l1;
+    }
+}
