a498b49b6c3168d215e23529adec6de32a07342a
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-12 16:45:55 +0530

    Kadanes Algorithm...

--- src/main/java/Main/MaximumSubarray.java
+++ src/main/java/Main/MaximumSubarray.java
@@ -2,17 +2,15 @@ package Main;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int a=Integer.MIN_VALUE;
+        int max=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(sum>a){
-                a=sum;
-            }
+            max=Math.max(sum,max);
             if(sum<0){
                 sum=0;
             }
         }
-        return a;
+        return max;
     }
 }
