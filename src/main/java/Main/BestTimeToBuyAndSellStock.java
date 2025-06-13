e404ad883a9d79793010d4da869d17aa60b4c2e7
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-13 15:42:43 +0530

    Best Time To Buy And Sell Stock...

--- src/main/java/Main/BestTimeToBuyAndSellStock.java
+++ src/main/java/Main/BestTimeToBuyAndSellStock.java
@@ -2,20 +2,25 @@ package Main;
 
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
+        int buyPrice = prices[0];
+        int profit = 0;
 
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<buyPrice){
+                buyPrice=prices[i];
             }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
+            else{
+                int currProfit=prices-buyPrice;
+                profit=Math.max(currProfit,profit);
             }
         }
+        return  profit;
+    }
 
-        return op;
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock stock = new BestTimeToBuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum Profit: " + stock.maxProfit(prices));
+
     }
 }
--- src/main/java/Main/MaxConsecutiveOnes.java
+++ src/main/java/Main/MaxConsecutiveOnes.java
@@ -1,4 +1,19 @@
 package Main;
 
 public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+            } else {
+                maxCount = Math.max(maxCount, currentCount);
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
 }
