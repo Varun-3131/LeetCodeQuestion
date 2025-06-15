17e6cc9e43e929cec2fb8b47764675b19e165916
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-15 15:01:23 +0530

    Merge Intervals

--- src/main/java/Main/MergeIntervals.java
+++ src/main/java/Main/MergeIntervals.java
@@ -2,34 +2,28 @@ package Main;
 
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < intervals.length; i++) {
-            min = Math.min(min, intervals[i][0]);
-            max = Math.max(max, intervals[i][0]);
+        if(intervals.length<=1){
+            return intervals;
         }
+        //sort on basis of first index of all arrays
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        //New List created to store the result...
+        List<int[]> result=new ArrayList<>();
+        int[] newInterval=intervals[0];
+        //Add the first intervals...
+        result.add(newInterval);
 
-        int[] range = new int[max - min + 1];
-        for (int i = 0; i < intervals.length; i++) {
-            range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]);
-        }
-
-        int start = 0, end = 0;
-        LinkedList<int[]> result = new LinkedList<>();
-        for (int i = 0; i < range.length; i++) {
-            if (range[i] == 0) {
-                continue;
+        for(int[] interval:intervals){
+            //2<3
+            if(interval[0]<=newInterval[1]){
+                newInterval[1]=Math.max(newInterval[1],interval[1]);
             }
-            if (i <= end) {
-                end = Math.max(range[i], end);
-            } else {
-                result.add(new int[] {start + min, end + min});
-                start = i;
-                end = range[i];
+            else{
+                newInterval=interval;
+                result.add(newInterval);
             }
         }
-        result.add(new int[] {start + min, end + min});
         return result.toArray(new int[result.size()][]);
+
     }
 }
