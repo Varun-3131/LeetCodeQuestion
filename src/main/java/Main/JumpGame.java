fd717f5fbe6b8ef90a459a7544fcc3cb89d7a75e
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-15 14:21:25 +0530

    Jump Game...

--- src/main/java/Main/JumpGame.java
+++ src/main/java/Main/JumpGame.java
@@ -2,11 +2,14 @@ package Main;
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
-        int reachable = 0;
-        for(int i = 0; i < nums.length; i ++) {
-            if(i > reachable) return false;
-            reachable = Math.max(reachable, i + nums[i]);
+        public boolean canJump(int[] nums) {
+            int finalPos=nums.length-1;
+
+            for(int i=nums.length-2;i>=0;i--){
+                if(i+nums[i]>=finalPos){
+                    finalPos=i;
+                }
+            }
+            return finalPos==0;
         }
-        return true;
-    }
 }
