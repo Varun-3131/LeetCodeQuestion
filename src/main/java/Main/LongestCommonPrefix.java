d0e2fac6012aa854665fd9053e24664a80c0a51e
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-24 15:48:09 +0530

    Longest Common Prefix

--- src/main/java/Main/LongestCommonPrefix.java
+++ src/main/java/Main/LongestCommonPrefix.java
@@ -1,19 +1,21 @@
 package Main;
 import java.util.Arrays;
 public class LongestCommonPrefix {
-        public String longestCommonPrefix(String[] strs) {
-            Arrays.sort(strs);
-            String s1 = strs[0];
-            String s2 = strs[strs.length-1];
-            int idx = 0;
-            while(idx < s1.length() && idx < s2.length()){
-                if(s1.charAt(idx) == s2.charAt(idx)){
-                    idx++;
-                } else {
-                    break;
-                }
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result=new StringBuilder();
+        Arrays.sort(strs);
+        //First string...
+        char[] first=strs[0].toCharArray();
+        //Last String...
+        char[] last=strs[strs.length-1].toCharArray();
+        //Check First and last...
+        for(int i=0;i<first.length;i++){
+            if(first[i]!=last[i]){
+                break;
             }
-            return s1.substring(0, idx);
+            result.append(first[i]);
         }
+        return result.toString();
+    }
     }
 
