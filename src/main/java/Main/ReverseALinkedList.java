5518d5c9534bac211df223a3ac41f4ac4578b36e
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-27 16:38:42 +0530

    Reverse A Singly Linked List...

---
+++ src/main/java/Main/ReverseALinkedList.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class ReverseALinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode next=null;
+        ListNode prev=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
