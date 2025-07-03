e329e03b964b39d95709de40fe0a84285212836f
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-03 16:33:00 +0530

    Rotate List...

--- src/main/java/Main/RotateList.java
+++ src/main/java/Main/RotateList.java
@@ -2,81 +2,54 @@ package Main;
 
 public class RotateList {
     public ListNode rotateRight(ListNode head, int k) {
-        return rotateRightUsingCloseRing(head, k);
-    }
+        if (head == null || head.next == null || k == 0) return head;
 
-    private ListNode rotateRightUsingCloseRing(ListNode head, int k) {
 
-        if (head == null) {
-            return head;
-        }
-        int numberOfNodes = 1;
+        int len = 1;
         ListNode tail = head;
-
         while (tail.next != null) {
             tail = tail.next;
-            numberOfNodes++;
+            len++;
         }
-        k = k % numberOfNodes;
+
         tail.next = head;
 
-        for (int i = 0; i < numberOfNodes - k; i++) {
+        k = k % len;
+        int stepsToNewTail = len - k;
+
+        ListNode newTail = tail;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+
+        int len = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
             tail = tail.next;
-            head = head.next;
+            len++;
         }
-        tail.next = null;
 
-        return head;
-    }
+        tail.next = head;
 
-    private ListNode rotateRightUsingReverse(ListNode head, int k) {
-        int numberOfNodes = countNodes(head);
+        k = k % len;
+        int stepsToNewTail = len - k;
 
-        if (numberOfNodes == 0) {
-            return head;
+        ListNode newTail = tail;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
         }
-        ListNode reversedHead = reverse(head);
-        ListNode reversedTail = head;
 
-        k = k % numberOfNodes;
+        ListNode newHead = newTail.next;
+        newTail.next = null;
 
-        while (k > 0) {
-            ListNode toBeRotatedNode = reversedHead;
-            reversedHead = reversedHead.next;
-            toBeRotatedNode.next = null;
-
-            reversedTail.next = toBeRotatedNode;
-            reversedTail = toBeRotatedNode;
-
-            k--;
-        }
-        return reverse(reversedHead);
-    }
-
-    private ListNode reverse(ListNode node) {
-        ListNode nodeNext = null;
-        ListNode prev = null;
-
-        while (node != null) {
-            nodeNext = node.next;
-            node.next = prev;
-            prev = node;
-            node = nodeNext;
-        }
-        return prev;
-    }
-
-    private int countNodes(ListNode head) {
-
-        if (head == null) {
-            return 0;
-        }
-        int count = 0;
-
-        while (head != null) {
-            count++;
-            head = head.next;
-        }
-        return count;
+        return newHead;
     }
 }
