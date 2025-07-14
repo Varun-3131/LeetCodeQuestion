cc504e97483beb9ed587f9bda79f63b1b2787578
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-14 16:44:36 +0530

    Time Needed To Buy Tickets...

---
+++ src/main/java/Main/TimeNeededToBuyTickets.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class TimeNeededToBuyTickets {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time = time + Math.min(tickets[i], tickets[k]);
+            } else {
+                time = time + Math.min(tickets[i], tickets[k]-1);
+
+            }
+        }
+        return time;
+    }
+}
