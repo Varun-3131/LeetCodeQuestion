2295782a82f092b90bc9086fc3bb7117b12fe74e
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-15 16:55:53 +0530

    Min Stack...

--- src/main/java/Main/MinStack.java
+++ src/main/java/Main/MinStack.java
@@ -1,26 +1,24 @@
 package Main;
 
 public class MinStack {
-
-    private Stack<Integer> stack;
-    private Stack<Integer> minStack;
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
 
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
+        stack=new Stack<>();
+        minStack=new Stack<>();
     }
 
     public void push(int val) {
         stack.push(val);
 
-
-        val = Math.min(val, minStack.isEmpty() ? val : minStack.peek());
-        minStack.push(val);
+        if(minStack.isEmpty()||val<=minStack.peek()) minStack.push(val);
     }
 
     public void pop() {
-        stack.pop();
-        minStack.pop();
+        int popVal=stack.pop();
+
+        if(popVal==minStack.peek()) minStack.pop();
     }
 
     public int top() {
