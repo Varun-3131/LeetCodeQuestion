100a527cf0cf833c1635e4dd2f40284b8f5d54e5
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-30 18:17:54 +0530

    Combination Sum...

--- src/main/java/Main/CombinationSum.java
+++ src/main/java/Main/CombinationSum.java
@@ -1,29 +1,28 @@
-package Main;
+class Solution {
 
-public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> resultt = new ArrayList<List<Integer>>();
-        List<Integer>ds = new ArrayList<>();
-        int n = candidates.length;
-        int startIndex = 0;
-        recurCombinationSum(startIndex , candidates , target , n ,ds,resultt );
-
-        return resultt;
-
-    }
-
-    private void recurCombinationSum(int startIndex, int[] candidates, int target, int n, List<Integer> ds, List<List<Integer>> resultt) {
-        if (startIndex == n) {
-            if(target == 0){
-                resultt.add(new ArrayList<>(ds));
-                return;
-            }
+    void getAllComb(int[] arr, int idx, int target, List<List<Integer>> ans, List<Integer> comb) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(comb));
             return;
         }
-        ds.add(candidates[startIndex]);
-        if(candidates[startIndex] <= target)
-            recurCombinationSum(startIndex , candidates, target -candidates[startIndex], n ,ds,resultt);
-        ds.remove(ds.size() - 1);
-        recurCombinationSum(startIndex +1  , candidates, target,n,ds,resultt);
+
+        if (idx == arr.length || target < 0) {
+            return;
+        }
+
+        // Include current element
+        comb.add(arr[idx]);
+        getAllComb(arr, idx, target - arr[idx], ans, comb);  // multiple include
+        comb.remove(comb.size() - 1);  // backtrack
+
+        // Exclude current element
+        getAllComb(arr, idx + 1, target, ans, comb);
+    }
+
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> comb = new ArrayList<>();
+        getAllComb(arr, 0, target, ans, comb);
+        return ans;
     }
 }
