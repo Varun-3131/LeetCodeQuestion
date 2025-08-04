57414d5726be8023d02f16791b314defbe40e93d
Varun-3131 <srivastavvarun48@gmail.com>
2025-08-04 17:51:58 +0530

    Palindrome Partitioning...

--- src/main/java/Main/PalindromePartitioning.java
+++ src/main/java/Main/PalindromePartitioning.java
@@ -1,27 +1,31 @@
 package Main;
 
 public class PalindromePartitioning {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<List<String>>();
-        if (s.equals("")) {
-            res.add(new ArrayList<String>());
-            return res;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (isPalindrome(s, i + 1)) {
-                for (List<String> list : partition(s.substring(i + 1))) {
-                    list.add(0, s.substring(0, i + 1));
-                    res.add(list);
-                }
-            }
-        }
-        return res;
+    boolean isPalindrome(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        return s.equals(reversed);
     }
 
-    public boolean isPalindrome(String s, int n) {
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) return false;
+    void getAllParts(String s, List<String> partition, List<List<String>> ans) {
+        if (s.length() == 0) {
+            ans.add(new ArrayList<>(partition));
+            return;
         }
-        return true;
+
+        for (int i = 0; i < s.length(); i++) {
+            String part = s.substring(0, i + 1);
+            if (isPalindrome(part)) {
+                partition.add(part);
+                getAllParts(s.substring(i + 1), partition, ans);
+                partition.remove(partition.size() - 1);
+            }
+        }
+    }
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> partition = new ArrayList<>();
+        getAllParts(s, partition, ans);
+        return ans;
     }
 }
